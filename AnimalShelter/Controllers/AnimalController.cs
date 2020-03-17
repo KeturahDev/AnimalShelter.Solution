75c246841bb03db7be1522c13bcf56c3521cd7e4
Keturah Howard <keturah.dev@gmail.com>
2020-03-17 16:20:17 -0700

    getlisting views and detail view to display by passing model into ActionResult Index.
    Co-authored-by: Dana Luukko <dcluukkp@gmail.com>
    Co-authored-by: Stephanie Podolak <StephPodolak@gmail.com>

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -5,13 +5,38 @@ using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-  public class AnimalController : Controller
+  public class AnimalsController : Controller
   {
     private readonly AnimalShelterContext _db;
 
-    public AnimalController(AnimalShelter db)
+    public AnimalsController(AnimalShelterContext db)
     {
-        _db = db;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+    List<Animal> model = _db.Animals.ToList();
+    return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+    Animal thisAnimal = _db.Animals.FirstOrDefault(AnimalShelter => AnimalShelter.AnimalId == id);
+    return View(thisAnimal);
     }
   }
 }
